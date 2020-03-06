380a4e59445316f7a309b7862017f72a1e88896f
Kashiwara <kashiwara0205@tekitou.com>
2020-03-06 15:49:59 +0900

    Added prefix exporession test

--- parser/minitest.nim
+++ parser/minitest.nim
@@ -108,3 +108,46 @@ block integer_literal_test:
   var integer_literal = IntegerLiteral(ast.ExpressionStatement(statement).expression)
   test.eq_value("INT", integer_literal.tok.t_type)
   test.eq_value(5, integer_literal.number)
+
+# outline: whther be able to parse prefix_expression
+# expected_value: expected prefix_expression
+block prefix_expression_test:
+  var program = test.get_program("!5;")
+  test.eq_value(1, program.statements.len)
+  var statement = program.statements[0]
+  var prefix_expression = PrefixExpression(ast.ExpressionStatement(statement).expression)
+  test.eq_value("!", prefix_expression.tok.t_type)
+  test.eq_value("!", prefix_expression.operator)
+  var integer_literal = IntegerLiteral(prefix_expression.right)
+  test.eq_value("INT", integer_literal.tok.t_type)
+  test.eq_value(5, integer_literal.number)
+
+  program = test.get_program("-15;")
+  test.eq_value(1, program.statements.len)
+  statement = program.statements[0]
+  prefix_expression = PrefixExpression(ast.ExpressionStatement(statement).expression)
+  test.eq_value("-", prefix_expression.tok.t_type)
+  test.eq_value("-", prefix_expression.operator)
+  integer_literal = IntegerLiteral(prefix_expression.right)
+  test.eq_value("INT", integer_literal.tok.t_type)
+  test.eq_value(15, integer_literal.number)
+
+  program = test.get_program("!true")
+  test.eq_value(1, program.statements.len)
+  statement = program.statements[0]
+  prefix_expression = PrefixExpression(ast.ExpressionStatement(statement).expression)
+  test.eq_value("!", prefix_expression.tok.t_type)
+  test.eq_value("!", prefix_expression.operator)
+  var boolean = Boolean(prefix_expression.right)
+  test.eq_value("true", boolean.tok.t_type)
+  test.eq_value(true, boolean.value)
+
+  program = test.get_program("!false")
+  test.eq_value(1, program.statements.len)
+  statement = program.statements[0]
+  prefix_expression = PrefixExpression(ast.ExpressionStatement(statement).expression)
+  test.eq_value("!", prefix_expression.tok.t_type)
+  test.eq_value("!", prefix_expression.operator)
+  boolean = Boolean(prefix_expression.right)
+  test.eq_value("false", boolean.tok.t_type)
+  test.eq_value(false, boolean.value)
